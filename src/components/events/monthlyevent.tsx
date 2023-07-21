f80ea9bc4319361ca5aec10fbd5ee33ad0baa0a6
mickymoucs <mkycskwcst@gmail.com>
2023-07-21 23:47:34 +0700

    register

--- src/components/events/monthlyevent.tsx
+++ src/components/events/monthlyevent.tsx
@@ -75,7 +75,7 @@ const monthlyevent = () => {
       <Next/>
       <div className="grid grid-cols-6 md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-1 place-content-center p-3">
       {Eventcards.filter((eventcard) => ( eventcard.date.split('.')[1] === "01")).map((eventcard) => (
-        <a href= "/event" className="">
+        <a href="/events" className="">
         <Card image={eventcard.image} date={eventcard.date} name={eventcard.name} place={eventcard.place} />
         </a>
       ))}
--- src/components/events/next.tsx
+++ src/components/events/next.tsx
@@ -1,6 +1,7 @@
 import React , {useState} from 'react';
 import MaterialSymbolsArrowBackIosNew from '../icon/PreButton'
 import MaterialSymbolsArrowForwardIos from '../icon/ForwardButton'
+import MaterialSymbolsArrowBackIos from '../icon/BackwardButton'
 
 const ArrayMonth = [
     'JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE','JULY','AUGUST','SEPTEMBER','OCTOBER','NOVEMBER','DECEMBER'
@@ -12,7 +13,7 @@ const Next = ({}) => {
     return (
         <div className="flex justify-around p-4 ">
         <div className="flex text-xl font-montserrat font-bold items-center">
-            <button onClick={()=>setCurrent((current-1+12)%12)}><MaterialSymbolsArrowBackIosNew></MaterialSymbolsArrowBackIosNew></button>
+            <button onClick={()=>setCurrent((current-1+12)%12)}><MaterialSymbolsArrowBackIos></MaterialSymbolsArrowBackIos></button>
             <div className="text-xl font-montserrat pl-2">{ArrayMonth[((current-1+12)%12)]}</div>
         </div>
         <div className="text-2xl font-montserrat font-bold">{ArrayMonth[current]}</div>
