89f9242d1bf61524896fcc539563965b830750b6
mickymoucs <mkycskwcst@gmail.com>
2023-08-03 15:36:45 +0700

    Edit SignUp

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -46,15 +46,6 @@ const Signup = ({}) => {
                                         <input className="border-2 border-gray-300 rounded-md pl-2 w-full py-1" type="text" placeholder=""></input>
                                     </form>
                                 </div>
-                                <div className="flex flex-row">
-                                    <p className="font-montserrat">Password Confirmation</p>
-                                    <span className="text-red-600 ml-1">*</span>
-                                </div>
-                                <div className="flex flex-col">
-                                    <form className="flex">
-                                        <input className="border-2 border-gray-300 rounded-md pl-2 w-full py-1" type="text" placeholder=""></input>
-                                    </form>
-                                </div>
                                 <div className="flex flex-row w-full justify-between">
                                     <div className="flex flex-row w-2/5">
                                     <p className="font-montserrat">First Name</p>
